2976766a2f7800e9013c512f367bd37c3cb96d28
Documenter.jl <documenter@juliadocs.github.io>
2021-07-09 23:30:15 +0000

    build based on d313075

---
+++ v0.1.4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphMLDatasets","category":"page"},{"location":"#GraphMLDatasets","page":"Home","title":"GraphMLDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphMLDatasets]","category":"page"}]
+}
